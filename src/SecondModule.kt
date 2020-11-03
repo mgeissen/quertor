9ba52f9641db622ecb3c21b19bcfa58b7c2a46c9
mgeissen <matt.geis97@gmail.com>
2020-11-03 23:28:56 +0100

    add first interceptor

--- src/SecondModule.kt
+++ src/SecondModule.kt
@@ -1,11 +1,18 @@
 package de.matzemedia
 
 import io.ktor.application.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
 @Suppress("unused") // Referenced in application.conf
 fun Application.secondModule() {
+
+    intercept(ApplicationCallPipeline.Call) {
+        if (call.request.uri == "/www") {
+            call.respondText("I'm the interceptor!'")
+        }
+    }
 
     routing {
         get("/wow") {
